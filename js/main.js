01066fad056c94e10e9ff5a85cb88fa909a5de9c
Dmytro Riabtsun <riabtsun@i.ua>
2023-06-18 11:56:06 +0300

    added styles and functions for resume generating to pdf

--- js/main.js
+++ js/main.js
@@ -50,3 +50,56 @@ const scrollTop = () => {
 }
 
 window.addEventListener('scroll', scrollTop)
+
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+const scaleCv = () => {
+    document.body.classList.add('scale-cv')
+}
+
+const removeScale=()=>{
+    document.body.classList.remove('scale-cv')
+}
+
+let areaCv = document.getElementById('area-cv')
+let resumeBtn = document.getElementById('resume-button')
+
+let opt = {
+    margin:       0,
+    filename:     'myResume.pdf',
+    image:        { type: 'jpeg', quality: 0.98 },
+    html2canvas:  { scale: 4 },
+    jsPDF:        { format: 'a4', orientation: 'portrait' }
+};
+
+
+const generateResume =()=>{
+    html2pdf(areaCv, opt)
+}
+
+resumeBtn.addEventListener('click', ()=>{
+    scaleCv()
+    generateResume()
+    setTimeout(removeScale, 5000)
+})
